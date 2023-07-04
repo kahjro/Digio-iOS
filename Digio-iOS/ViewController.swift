0a3d33024530f90e44735463eba24865b1a49fae
Arthur Kazuo <artkazuye@gmail.com>
2023-07-04 20:08:30 -0300

    Initial Commit

---
+++ Digio-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Digio-iOS
+//
+//  Created by Kazuo on 04/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
